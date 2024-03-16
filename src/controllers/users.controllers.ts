b23df477b346337514185b0350434070322d7dc8
nlara <noe_lara99@outlook.com>
2024-03-15 18:33:08 -0600

    feat: :zap: Endpoints: FindOne, Update User & Delete User

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -12,7 +12,36 @@ export const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await USERS.find();
     defaultResponse.data = users;
-    defaultResponse.message = "all users list";
+    defaultResponse.message = "All users list";
+    defaultResponse.success = true;
+
+    return res.status(200).json(defaultResponse);
+  } catch (error) {
+    if (error instanceof Error) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = error.message;
+
+      return res.status(500).json(defaultResponse);
+    }
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await USERS.findOne({ where: { id } });
+
+    if (!user) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = "User not found";
+
+      return res.status(404).json(defaultResponse);
+    }
+
+    defaultResponse.data = user;
+    defaultResponse.message = "User found";
     defaultResponse.success = true;
 
     return res.status(200).json(defaultResponse);
@@ -29,9 +58,7 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
     const { username, password, email } = req.body;
-
 
     const user = new USERS();
     user.username = username;
@@ -40,7 +67,74 @@ export const createUser = async (req: Request, res: Response) => {
     await user.save();
 
     defaultResponse.data = user;
-    defaultResponse.message = "user created successfully";
+    defaultResponse.message = "User created successfully";
+    defaultResponse.success = true;
+
+    return res.status(200).json(defaultResponse);
+  } catch (error) {
+    if (error instanceof Error) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = error.message;
+
+      return res.status(500).json(defaultResponse);
+    }
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username, password, email } = req.body;
+
+    const user = await USERS.findOne({ where: { id } });
+    if (!user) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = "User not found";
+
+      return res.status(404).json(defaultResponse);
+    }
+
+    user.username = username || user.username;
+    user.password = password || user.password;
+    user.email = email || user.email;
+
+    await user.save();
+
+    defaultResponse.data = user;
+    defaultResponse.message = "User updated successfully";
+    defaultResponse.success = true;
+
+    return res.status(200).json(defaultResponse);
+  } catch (error) {
+    if (error instanceof Error) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = error.message;
+
+      return res.status(500).json(defaultResponse);
+    }
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = await USERS.findOne({ where: { id } });
+    if (!user) {
+      defaultResponse.data = null;
+      defaultResponse.success = false;
+      defaultResponse.message = "User not found";
+
+      return res.status(404).json(defaultResponse);
+    }
+
+    await user.remove();
+
+    defaultResponse.data = null;
+    defaultResponse.message = "User deleted successfully";
     defaultResponse.success = true;
 
     return res.status(200).json(defaultResponse);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users.controllers";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/users.controllers";
 
 const router = Router();
 
-router.get("/users", getUsers);
-router.post("/users", createUser);
+router.get("/users", getUsers); // Endpoint para obtener todos los usuarios
+router.get("/users/:id", getUserById); // Endpoint para obtener un usuario por ID
+router.post("/users", createUser); // Endpoint para crear un nuevo usuario
+router.put("/users/:id", updateUser); // Endpoint para actualizar un usuario por ID
+router.delete("/users/:id", deleteUser); // Endpoint para eliminar un usuario por ID
 
 export default router;
